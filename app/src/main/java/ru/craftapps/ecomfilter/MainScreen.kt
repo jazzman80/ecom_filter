9056bcab84068f3ad2f8839fce2bde1dd1630f47
Иван <jazzman80@yandex.ru>
2024-01-02 22:38:48 +0300

    Главный экран

--- app/src/main/java/ru/craftapps/ecomfilter/MainScreen.kt
+++ app/src/main/java/ru/craftapps/ecomfilter/MainScreen.kt
@@ -1,6 +1,31 @@
 package ru.craftapps.ecomfilter
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun MainScreen()
+fun MainScreen(){
+    BasicText(
+        modifier = Modifier
+            .background(
+                color = Color.White
+            )
+            .fillMaxSize(),
+        text = "Hello World!",
+        style = TextStyle(
+            color = Color.Black
+        )
+    )
+}
+
+@Preview
+@Composable
+fun MainScreenPreview(){
+    MainScreen()
+}
